ded6c91aa33ca607afa6f0016ef16379562c37af
Jakub Szubka <jakubszubkawork@gmail.com>
2026-03-10 10:36:22 +0100

    Ad Tasks DbSet to ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TaskManagerAuth.Models;
 
 namespace TaskManagerAuth.Data
 {
@@ -9,5 +10,7 @@ namespace TaskManagerAuth.Data
             : base(options)
         {
         }
+
+        public DbSet<TodoTask> Tasks { get; set; }
     }
 }
