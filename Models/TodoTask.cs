302bac1edee363615f0d6f7fd39d03e9eb555c12
Jakub Szubka <jakubszubkawork@gmail.com>
2026-03-10 09:42:47 +0100

    Add TodoTask model with user ownership

---
+++ Models/TodoTask.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerAuth.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public string UserId { get; set; }
+
+    }
+}
