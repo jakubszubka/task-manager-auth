82d398e245b9232f0ce3e17f3e2afc20929548f7
Jakub Szubka <jakubszubkawork@gmail.com>
2026-03-10 12:16:40 +0100

    Switch to SQLite; add initial migration for Tasks and Identity tables

--- Program.cs
+++ Program.cs
@@ -10,14 +10,10 @@ namespace TaskManagerAuth
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter(); // optional for dev
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
