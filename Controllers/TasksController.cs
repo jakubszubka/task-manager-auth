4f7483e2c2d0a22a364b11f357d259c93f3470f0
Jakub Szubka <jakubszubkawork@gmail.com>
2026-03-11 11:26:57 +0100

    Make tasks user-specific: filter by current user and assign UserId on create

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerAuth.Data;
+using TaskManagerAuth.Models;
+
+namespace TaskManagerAuth.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TasksController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var tasks = await _context.Tasks
+                .Where(t => t.UserId == userId)
+                .ToListAsync();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TodoTask task)
+        {
+            if (!ModelState.IsValid)
+            { return View(task); }
+
+            task.UserId = _userManager.GetUserId(User);
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
